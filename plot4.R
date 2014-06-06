1b62ad58d4ce5a0698060715692ce5d69927a913
chrameshkumar <ch.rameshkumar@gmail.com>
2014-06-06 21:20:42 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,45 @@
+library(data.table)
+##Downloading zip fileand extracting data text file from link if required input text file does not exist in working directory
+if(!file.exists("./household_power_consumption.txt")){
+  if(!file.exists("./household_power_consumption.zip")){
+    fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip" 
+    download.file(fileUrl,destfile = "./household_power_consumption.zip")
+    unlink(fileUrl)
+  }
+  
+  unzip("./household_power_consumption.zip",files ="household_power_consumption.txt")
+}
+
+##Loading input data from text data file as data.table 
+data.table(read.table("household_power_consumption.txt", header = TRUE, sep = ";",stringsAsFactor = F))->tbl_data
+
+
+##capturing data for  required dates
+dates_req<-as.Date(c("01/02/2007","02/02/2007"),format="%d/%m/%Y")
+as.Date(dates_req,"%d/%m/%Y")->dates_req
+tbl_data[as.Date(strptime(tbl_data$Date,format="%d/%m/%Y")) %between% dates_req]->req_dat_all
+
+
+paste(as.character(as.Date(req_dat_all$Date,format("%d/%m/%Y"))),as.character(req_dat_all$Time),sep =" ")->Datetime
+
+data.table(Datetime=as.POSIXct(Datetime),Global_active_power=req_dat_all$Global_active_power,Global_reactive_power = req_dat_all$Global_reactive_power,Voltage=req_dat_all$Voltage,Sub_metering_1=req_dat_all$Sub_metering_1,Sub_metering_2=req_dat_all$Sub_metering_2,Sub_metering_3=req_dat_all$Sub_metering_3)->req_detail
+
+req_detail[complete.cases(as.numeric(Global_active_power),as.numeric(Global_reactive_power),as.numeric(Voltage),as.numeric(Sub_metering_1),as.numeric(Sub_metering_2),as.numeric(Sub_metering_3))]->req_xy
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
+par(mfrow = c(2, 2))
+with(req_xy, {
+  plot(Datetime,Global_active_power, type = "l",xlab ="")
+  plot(Datetime,Voltage,type = "l")
+  
+  plot(Datetime,as.numeric(Sub_metering_1),type ="l",xlab ="",col ="black",ylab="EnergySubmetering")
+  par(col ="red")
+  lines(Datetime,as.numeric(Sub_metering_2,col = "red",xlab = "Datetime)"))
+  par(col ="blue")
+  lines(Datetime,as.numeric(Sub_metering_3, col = "blue"))
+  par(col="black")
+  legend("topright",bty = "n", lty =1,col = c("black", "red",  "blue","black"),legend = c("sub_metering_1", "sub_metering_2",  "sub_metering_3"))##plot(as.POSIXct(Datetime),Sub_metering_1, type = "l")
+  plot(Datetime,Global_reactive_power, type = "l",col = "black",xlab= "Datetime")
+    
+})
+dev.off() 
+
